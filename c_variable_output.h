dbe3a11750974cb116cf6236d0fe5d6a7a487c69
tesusoft <tesukidney@gmail.com>
2023-03-18 12:06:38 +0100

    Added new code ot c output file

--- c_variable_output.h
+++ c_variable_output.h
@@ -10,5 +10,14 @@ int main() {
   printf("%d\n", myNum);
   printf("%f\n", myFloatNum);
   printf("%c\n", myLetter);
+// To combine both text and a variable, separate them with a comma inside the printf()
+  int myNum1 = 15;
+  printf("My favorite number is: %d", myNum1);
   return 0;
+
+  // To print multiple data format in one printfLine
+  char myLetter = 'D';
+  printf("My number is %d and my letter is %c", myNum1, myLetter);
+  return 0;
+
 }
